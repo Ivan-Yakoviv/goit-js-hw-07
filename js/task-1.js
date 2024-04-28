52a292e8735e491d91e60ae1e8e2d61060cf05d2
Ivan-Yakoviv <ivasykvv@gmail.com>
2024-04-29 01:24:58 +0300

    add 4

--- js/task-1.js
+++ js/task-1.js
@@ -1,15 +1,33 @@
 const mainList = document.querySelectorAll(".item");
-// mainList.style.listStyle = "none";
-console.log(mainList.length);
+
 console.log(`Number of categories: ${mainList.length}`);
 mainList.forEach(function (item) {
     const title = item.querySelector(".title");
+    const list = item.querySelector("ul");
+    console.log(`Categorie: ${title.textContent}`);
+    const name = list.querySelectorAll("li");
+    console.log(`Elements: ${name.length}`);
+    name.forEach(function (nameItem) {
+        // const nameItemText = nameItem.querySelector("li");
+        nameItem.style.border = "1px solid #808080";
+        nameItem.style.borderRadius = "4px";
+        nameItem.style.width = "360px";
+        nameItem.style.height = "40px";
+        nameItem.style.flexShrink = "0";
+        nameItem.style.fontSize = "16px";
+        nameItem.style.lineHeight = "1.5";
+        nameItem.style.letterSpacing = "0.04em";
+        nameItem.style.boxSizing = "borderBox";
+        nameItem.style.paddingTop = "8px";
+        nameItem.style.paddingBottom = "8px";
+        nameItem.style.paddingLeft = "16px";
+    });
+    
+    
     title.style.margin = "0";
     title.style.fontSize = "24px";
     title.style.lineHeight = "1.33"; /* 133.333% */
     title.style.letterSpacing = "0.04em";
-    const list = item.querySelector("ul");
-    
     list.style.padding = "0";
     list.style.listStyle = "none";
     list.style.width = "360px";
@@ -24,9 +42,6 @@ mainList.forEach(function (item) {
     list.parentElement.style.gap = "16px";
     list.parentElement.style.borderRadius = "8px";
     list.parentElement.style.padding = "16px";
-    console.log(`Categorie: ${title.textContent}`);
-        const name = item.querySelectorAll("li");
-        console.log(`Elements: ${name.length}`);
     });
     
     // name.classList.add("name");
@@ -35,4 +50,4 @@ mainList.forEach(function (item) {
     // return item;
 const adList = document.querySelector("#categories");
 console.log(adList.innerHTML);
-adList.style.justifyContent = "center";
+// adList.style.justifyContent = "center";
--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,73 @@
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", handSubmit);
+
+function handSubmit(event) {
+    event.preventDefault();
+
+    const elements = event.currentTarget.elements;
+
+    const info = {
+        email: elements.email.value,
+        password: elements.password.value,
+    }
+
+    if (elements.email.value === "" || elements.password.value === "") {
+        alert('All form fields must be filled in');
+        return false;
+    }
+
+    console.log(info);
+
+    event.currentTarget.reset()
+};
+
+const label = document.querySelector("label");
+const button = document.querySelector("button");
+const input = document.querySelector("input")
+
+form.style.display = "flex";
+form.style.width = "408px";
+form.style.flexDirection = "column";
+form.style.gap = "8px";
+form.style.padding = "24px";
+
+label.style.display = "flex";
+label.style.width = "360px";
+// label.style.height = "40px";
+label.style.flexDirection = "column";
+label.style.alignItems = "flex-start";
+label.style.gap = "8px";
+label.style.fontSize = "16px";
+label.style.lineHeight = "1.5"; /* 150% */
+label.style.letterSpacing = "0.04em";
+
+input.style.display = "flex";
+input.style.width = "360px";
+input.style.height = "40px";
+input.style.justifyContent = "center";
+input.style.alignItems = "center";
+input.style.color = "#808080";
+input.style.fontSize = "16px";
+
+button.style.display = "flex";
+button.style.width = "86px";
+button.style.height = "40px";
+button.style.padding = "8px 16px";
+button.style.justifyContent = "center";
+button.style.alignItems = "center";
+button.style.gap = "10px";
+button.style.borderRadius = "8px";
+button.style.background = "#4E75FF";
+button.style.color = "#FFF";
+button.style.fontSize = "16px";
+button.style.fontWeight = "500";
+button.style.lineHeight = "1.5"; /* 150% */
+button.style.letterSpacing = "0.04em";
+
+
+
+console.log(form.innerHTML);
+
+
 
